ce34c7f74e705ebb0417130d39d28eadc7eb137e
DHRUV KUMAR <dkiphones620@gmail.com>
2017-08-17 00:30:08 +0530

    refine

--- js/canvasBuilder.js
+++ js/canvasBuilder.js
@@ -1,35 +1,77 @@
-var c = document.querySelector("#c");
-var ctx = c.getContext("2d");
+function textChangeListener (evt) {
+      var id = evt.target.id;
+      var text = evt.target.value;
 
-/*var image = new Image();
+      if (id == "topLineText") {
+        window.topLineText = text;
+      } else {
+        window.bottomLineText = text;
+      }
 
-image.onload = function(){
-    console.log("Loaded image");
-    ctx.drawImage(image,0,0,c.width,c.height);
-    var savedImage = c.toDataURL();
-    window.open(savedImage);
-    ;
-}
-image.src="images/canvas.png";
+      redrawMeme(window.imageSrc, window.topLineText, window.bottomLineText);
+    }
 
- ctx.fillStyle = "blue";
- ctx.strokeStyle ="green";
- ctx.strokeRect(50,50,100,100);
- ctx.beginPath();
- ctx.moveTo(75,75);
- ctx.lineTo(125,125);
- ctx.lineTo(125,75);
- ctx.fill();*/
- //ctx.clearRect(0, 0, c.width, c.height);//this
- //c.width = c.width;//and this can be used to clear entire canvas
- ctx.font = "36pt Impact";
- ctx.textAlign = "center";
+    function redrawMeme(image, topLine, bottomLine) {
+      // Get Canvas2DContext
+      var canvas = document.querySelector('canvas');
+      var ctx = canvas.getContext("2d");
+      if (image != null){
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+      }
 
- ctx.fillStyle = "white";
- ctx.fillText("CANVAS MEMES!",c.width / 2, 40);
+      // Text attributes
+      ctx.font = '30pt Impact';
+      ctx.textAlign = 'center';
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 3;
+      ctx.fillStyle = 'white';
 
- ctx.strokeStyle = "black";
- ctx.lineWidth = 3;
- ctx.strokeText("CANVAS MEMES!",c.width / 2, 40);
+      if (topLine != null) {
+        ctx.fillText(topLine, canvas.width / 2, 40);
+        ctx.strokeText(topLine, canvas.width / 2, 40);
+      }
+
+      if (bottomLine != null) {
+        ctx.fillText(bottomLine, canvas.width / 2, canvas.height - 20);
+        ctx.strokeText(bottomLine, canvas.width / 2, canvas.height - 20);
+      }
+    }
+
+    function saveFile() {
+      window.open(document.querySelector('canvas').toDataURL());
+    }
+
+    function handleFileSelect(evt) {
+      var canvasWidth = 500;
+      var canvasHeight = 500;
+      var file = evt.target.files[0];
 
 
+
+      var reader = new FileReader();
+      reader.onload = function(fileObject) {
+        var data = fileObject.target.result;
+
+        // Create an image object
+        var image = new Image();
+        image.onload = function() {
+
+          window.imageSrc = this;
+          redrawMeme(window.imageSrc, null, null);
+        }
+
+        // Set image data to background image.
+        image.src = data;
+        console.log(fileObject.target.result);
+      };
+      reader.readAsDataURL(file)
+    }
+
+    window.topLineText = "";
+    window.bottomLineText = "";
+    var input1 = document.getElementById('topLineText');
+    var input2 = document.getElementById('bottomLineText');
+    input1.oninput = textChangeListener;
+    input2.oninput = textChangeListener;
+    document.getElementById('file').addEventListener('change', handleFileSelect, false);
+    document.querySelector('button').addEventListener('click', saveFile, false);
